087c5e7bffa93db50a124ef892f4b3cadd8c741e
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2021-09-30 17:11:46 +0900

    comment: tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
-const postcss_import = require('postcss-import')
-const tailwindcss = require('tailwindcss')
+// const postcss_import = require('postcss-import')
+// const tailwindcss = require('tailwindcss')
 const autoprefixer = require('autoprefixer')
 const purgecss = require('@fullhuman/postcss-purgecss')
-const cssnano = require('cssnano')
+// const cssnano = require('cssnano')
 
 module.exports = {
   plugins: [
-    postcss_import,
-    tailwindcss,
+    // postcss_import,
+    // tailwindcss,
     autoprefixer,
     purgecss({ content: ['./src/**/*.html']}),
     // cssnano({ preset: 'default'})
